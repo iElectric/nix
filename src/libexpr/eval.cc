59167d47eb9da9d2efe190372aa0e5e670ac4846
Domen Kožar <domen@cachix.org>
2024-09-27 12:51:45 +0100

    remove inactionable warning

--- src/libexpr/eval.cc
+++ src/libexpr/eval.cc
@@ -2370,11 +2370,6 @@ StorePath EvalState::copyPathToStore(NixStringContext & context, const SourcePat
 std::string EvalState::computeBaseName(const SourcePath & path)
 {
     if (path.path.isRoot()) {
-        warn(
-            "Performing inefficient double copy of path '%s' to the store. "
-            "This can typically be avoided by rewriting an attribute like `src = ./.` "
-            "to `src = builtins.path { path = ./.; name = \"source\"; }`.",
-            path);
         return std::string(fetchToStore(*store, path, FetchMode::DryRun).to_string());
     } else
         return std::string(path.baseName());
